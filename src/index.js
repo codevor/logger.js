8d5de486ea95983f651fdc97a079ab1dd7d40d6c
Helder Burato Berto <helder.burato@gmail.com>
2020-01-14 19:41:27 -0300

    refactor(index): export Logger and Emoji IIFE

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
-import { intercept } from './utils';
+import Logger from './logger';
 
 export { default as Emoji } from './emoji';
 
-export default class Logger {
-  static success(args) {
-    return intercept(this.success.name, args);
-  }
-
-  static info(args) {
-    return intercept(this.info.name, args);
-  }
-
-  static warn(args) {
-    return intercept(this.warn.name, args);
-  }
-
-  static error(args) {
-    return intercept(this.error.name, args);
-  }
-
-  static trace(args) {
-    return intercept(this.trace.name, args);
-  }
-
-  static debug(args) {
-    return intercept(this.debug.name, args);
-  }
-}
+export default Logger;
