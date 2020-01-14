c3fdcb13d407a2b4b5531aa715ea5f1e8075cab7
Helder Burato Berto <helder.burato@gmail.com>
2020-01-14 19:39:23 -0300

    feat: Logger moved to logger.js

---
+++ src/logger.js
@@ -0,0 +1,14 @@
+import { intercept } from './utils';
+
+const Logger = (() => {
+  return {
+    success: args => intercept('success', args),
+    info: args => intercept('info', args),
+    warn: args => intercept('warn', args),
+    error: args => intercept('error', args),
+    trace: args => intercept('trace', args),
+    debug: args => intercept('debug', args),
+  }
+})();
+
+export default Logger;
